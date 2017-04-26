b0bbc426525789def241ac3bc68496d5ef67a95d
Eric <emiller@tax-guard.com>
2017-04-26 11:41:34 -0600

    Added withoutOverlap() to the parsing of the articles job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('articles:gather')->everyMinute();
+        $schedule->command('articles:gather')->withoutOverlapping()->everyMinute();
     }
 
     /**
